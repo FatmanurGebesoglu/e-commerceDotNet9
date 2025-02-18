3787656bf6196de393692e470db7cc3b967e60af
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2025-02-18 21:34:31 +0300

    DataContext added

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext: DbContext
+{
+    public DbSet<Product> Products => Set<Product>();
+}
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82444b408be0c62ab656db5fa9ee56480f5d7993")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
